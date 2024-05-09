31eab64758343010c6e827294b2bf8a56ce700d4
Roman <129329752+rchernyshov@users.noreply.github.com>
2024-05-09 18:54:25 +0300

    Update app/src/main/java/com/bogdash/cocktails/home/FragmentHomeScreen.kt
    Co-authored-by: Anastasia Bogdanova <ms.anastasia.bogdanova@gmail.com>

--- app/src/main/java/com/bogdash/cocktails/home/FragmentHomeScreen.kt
+++ app/src/main/java/com/bogdash/cocktails/home/FragmentHomeScreen.kt
@@ -11,7 +11,6 @@ class FragmentHomeScreen : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
