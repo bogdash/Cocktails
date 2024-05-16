c5ef29ad1af96a7475f6fc2320b4a6a012e1f2b0
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-05-16 21:19:03 +0300

    Update constants for network

--- data/src/main/java/com/bogdash/data/storage/network/ConstantsForNetwork.kt
+++ data/src/main/java/com/bogdash/data/storage/network/ConstantsForNetwork.kt
@@ -1,9 +1,5 @@
 package com.bogdash.data.storage.network
 
 object ConstantsForNetwork {
-    class Network {
-        companion object {
-            const val BASE_URL = "www.thecocktaildb.com/api/json/v1/1/"
-        }
-    }
+    const val BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/"
 }
