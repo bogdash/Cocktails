fc9b5fed31740924525d7ccdb0666ad4caca8134
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-05-23 14:21:09 +0300

    Update gradle in data module

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -37,12 +37,15 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Domain module
     implementation(project(":domain"))
+
     //OkHttp
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation(libs.okhttp)
+    implementation(libs.okhttp3.logging.interceptor)
 
     //Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation(libs.retrofit)
+    implementation(libs.retrofit2.converter.gson)
 }
