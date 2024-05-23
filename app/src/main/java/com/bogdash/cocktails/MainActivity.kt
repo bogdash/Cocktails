e8bc8a9ea71f1884b9aed310a8b808fcb5ab5d05
Alina <alina.goncharova.ok@gmail.com>
2024-05-23 14:54:18 +0300

    design MainActivity, add bottomNavBar, fix designing search screen, fix splash screen

--- app/src/main/java/com/bogdash/cocktails/MainActivity.kt
+++ app/src/main/java/com/bogdash/cocktails/MainActivity.kt
@@ -25,7 +25,6 @@ class MainActivity : AppCompatActivity() {
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            //v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             v.setPadding(0, systemBars.top, 0, 0)
             insets
         }
