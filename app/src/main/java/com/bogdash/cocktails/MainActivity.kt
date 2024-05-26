1eddb9cfaeb5e5d19cf5cbce288b4c067f2cfd9d
Roman <tchernyshoff.roman2015@gmail.com>
2024-05-26 18:55:16 +0300

    - Add Logs

--- app/src/main/java/com/bogdash/cocktails/MainActivity.kt
+++ app/src/main/java/com/bogdash/cocktails/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.bogdash.cocktails
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.bogdash.cocktails.databinding.DetailedFragmentBinding
+
 
 class MainActivity : AppCompatActivity() {
     //private lateinit var binding: ActivityMainBinding
@@ -14,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         binding = DetailedFragmentBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Log.d("MainActivity", "onCreate called")
 
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
--- app/src/main/java/com/bogdash/cocktails/detail/DetailedFragment.kt
+++ app/src/main/java/com/bogdash/cocktails/detail/DetailedFragment.kt
@@ -1,6 +1,7 @@
 package com.bogdash.cocktails.detail
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -22,13 +23,15 @@ class DetailedFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = DetailedFragmentBinding.inflate(inflater, container, false)
+    ): View {
+        Log.d("DetailedFragment", "onCreateView called")
+        binding = DetailedFragmentBinding.inflate(inflater, container, true)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.d("DetailedFragment", "onViewCreated called")
 
         // Установка начального фрагмента
         childFragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragmentList[0]).commit()
