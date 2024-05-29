abb9469fa010014e4bdb8067ccd3602aca039c88
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-05-29 16:15:59 +0300

    Update MainViewModel

--- app/src/main/java/com/bogdash/cocktails/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/bogdash/cocktails/presentation/main/MainViewModel.kt
@@ -1,3 +1,36 @@
 package com.bogdash.cocktails.presentation.main
 
-class MainViewModel {}
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bogdash.domain.models.Drink
+import com.bogdash.domain.usecases.GetCocktailOfTheDayUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val getCocktailOfTheDayUseCase: GetCocktailOfTheDayUseCase
+) : ViewModel() {
+
+    private var resultLiveDataMutable = MutableLiveData<String>()
+    val resultLiveData: LiveData<String> = resultLiveDataMutable
+
+    private val cocktailOfTheDayMutable = MutableLiveData<Drink>()
+    val resultCocktailOfTheDay: LiveData<Drink> = cocktailOfTheDayMutable
+
+    fun getCocktailOfTheDay() {
+        viewModelScope.launch {
+            try {
+                val cocktails = getCocktailOfTheDayUseCase.execute()
+                cocktailOfTheDayMutable.value = cocktails.drinks.first()
+            } catch (e: Exception) {
+                Log.d("MyLog", "cocktailOfTheDay error: $e")
+            }
+        }
+    }
+
+}
