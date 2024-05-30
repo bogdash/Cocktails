4d6c321c529f02b66f986630fc961f76c41123aa
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-05-30 16:19:14 +0300

    Update with using mappers

--- data/src/main/java/com/bogdash/data/repository/CocktailRepositoryImplementation.kt
+++ data/src/main/java/com/bogdash/data/repository/CocktailRepositoryImplementation.kt
@@ -1,5 +1,45 @@
 package com.bogdash.data.repository
 
+import com.bogdash.data.storage.network.ConstantsForNetwork.DATE_FORMAT_PATTERN
+import com.bogdash.data.storage.network.retrofit.CocktailsApiService
+import com.bogdash.data.storage.preferences.CocktailPreferences
+import com.bogdash.domain.models.Cocktails
 import com.bogdash.domain.repository.CocktailRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
-class CocktailRepositoryImplementation : CocktailRepository
+class CocktailRepositoryImplementation(
+    private val cocktailApiService: CocktailsApiService,
+    private val cocktailPreferences: CocktailPreferences
+) : CocktailRepository {
+
+    private val dateFormat = SimpleDateFormat(DATE_FORMAT_PATTERN, Locale.getDefault())
+
+    override suspend fun getCocktailOfTheDay(): Cocktails {
+        return withContext(Dispatchers.IO) {
+            val today = dateFormat.format(Date())
+
+            val lastUpdateDate = cocktailPreferences.getLastUpdateDate()
+            val savedDrink = cocktailPreferences.getCocktail()?.toDomain()
+
+            if (lastUpdateDate == today && savedDrink != null) {
+                Cocktails(listOf(savedDrink))
+            } else {
+                val dataCocktails = cocktailApiService.getCocktailOfTheDay()
+                val domainCocktails = Cocktails(dataCocktails.drinks.map { it.toData().toDomain() })
+
+                val drink = dataCocktails.drinks.firstOrNull()
+                if (drink != null) {
+                    cocktailPreferences.saveCocktail(drink.toData())
+                    cocktailPreferences.saveLastUpdateDate(today)
+                }
+
+                domainCocktails
+            }
+        }
+    }
+
+}
