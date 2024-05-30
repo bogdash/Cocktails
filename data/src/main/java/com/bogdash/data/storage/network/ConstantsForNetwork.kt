f7316904dd25fb182535c3d1aaa46ceb2581f5f8
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-05-30 16:14:25 +0300

    Update constantsFprNetwork

--- data/src/main/java/com/bogdash/data/storage/network/ConstantsForNetwork.kt
+++ data/src/main/java/com/bogdash/data/storage/network/ConstantsForNetwork.kt
@@ -2,4 +2,5 @@ package com.bogdash.data.storage.network
 
 internal object ConstantsForNetwork {
     const val BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/"
+    const val DATE_FORMAT_PATTERN = "yyyy-MM-dd"
 }
