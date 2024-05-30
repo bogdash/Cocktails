5b10cb96fdb4264eb75e37b73134e9139fa7ddee
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-05-30 16:15:23 +0300

    Update models in data module

--- data/src/main/java/com/bogdash/data/storage/models/Drink.kt
+++ data/src/main/java/com/bogdash/data/storage/models/Drink.kt
@@ -13,7 +13,28 @@ data class Drink(
     val ingredients: List<Ingredient>,
     val creativeCommonsConfirmed: String?,
     val dateModified: String
-)
+) {
+
+    fun toDomain(): com.bogdash.domain.models.Drink {
+        val ingredients = ingredients.map {
+            com.bogdash.domain.models.Ingredient(
+                it.name,
+                it.measure
+            )
+        }
+
+        return com.bogdash.domain.models.Drink(
+            id,
+            name,
+            category,
+            alcoholic,
+            glass,
+            instructions,
+            thumb,
+            ingredients
+        )
+    }
+}
 
 data class Ingredient(
     val name: String,
