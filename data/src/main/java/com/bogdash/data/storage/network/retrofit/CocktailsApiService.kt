45bb28215480defe1513a5e7c6a920025a853528
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-05-30 16:17:33 +0300

    Update getCocktailOfTheDay

--- data/src/main/java/com/bogdash/data/storage/network/retrofit/CocktailsApiService.kt
+++ data/src/main/java/com/bogdash/data/storage/network/retrofit/CocktailsApiService.kt
@@ -1,3 +1,9 @@
 package com.bogdash.data.storage.network.retrofit
 
-interface CocktailsApiService {}
+import com.bogdash.data.storage.network.dto.CocktailsDto
+import retrofit2.http.GET
+
+interface CocktailsApiService {
+    @GET("random.php")
+    suspend fun getCocktailOfTheDay(): CocktailsDto
+}
