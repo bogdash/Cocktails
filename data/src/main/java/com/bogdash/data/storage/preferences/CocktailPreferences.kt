97508c2f5b381e34fcfa3a09189d79dde53aa7eb
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-05-30 16:17:53 +0300

    Update CocktailPreferences

--- data/src/main/java/com/bogdash/data/storage/preferences/CocktailPreferences.kt
+++ data/src/main/java/com/bogdash/data/storage/preferences/CocktailPreferences.kt
@@ -1,6 +1,7 @@
 package com.bogdash.data.storage.preferences
 
 import com.bogdash.data.storage.models.Drink
+import com.bogdash.data.storage.network.dto.DrinkDto
 
 interface CocktailPreferences {
     fun saveCocktail(drink: Drink)
