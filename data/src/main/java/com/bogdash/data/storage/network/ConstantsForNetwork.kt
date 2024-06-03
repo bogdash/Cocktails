589f210034d1083085a1df2dca2f36e2b04c0bfb
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-06-03 12:55:44 +0300

    Extract constant

--- data/src/main/java/com/bogdash/data/storage/network/ConstantsForNetwork.kt
+++ data/src/main/java/com/bogdash/data/storage/network/ConstantsForNetwork.kt
@@ -3,4 +3,5 @@ package com.bogdash.data.storage.network
 internal object ConstantsForNetwork {
     const val BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/"
     const val DATE_FORMAT_PATTERN = "yyyy-MM-dd"
+    const val RANDOM = "random.php"
 }
--- data/src/main/java/com/bogdash/data/storage/network/retrofit/CocktailsApiService.kt
+++ data/src/main/java/com/bogdash/data/storage/network/retrofit/CocktailsApiService.kt
@@ -1,9 +1,10 @@
 package com.bogdash.data.storage.network.retrofit
 
+import com.bogdash.data.storage.network.ConstantsForNetwork.RANDOM
 import com.bogdash.data.storage.network.dto.CocktailsDto
 import retrofit2.http.GET
 
 interface CocktailsApiService {
-    @GET("random.php")
+    @GET(RANDOM)
     suspend fun getCocktailOfTheDay(): CocktailsDto
 }
