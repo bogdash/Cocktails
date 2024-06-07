2df0558ef0d99c64f2d6ec1f8ac495944768ef48
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-06-07 22:26:17 +0300

    Add entities

---
+++ data/src/main/java/com/bogdash/data/storage/database/entities/DrinkEntity.kt
@@ -0,0 +1,36 @@
+package com.bogdash.data.storage.database.entities
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "drink")
+data class DrinkEntity (
+    @PrimaryKey val id: String,
+    val name: String?,
+    val tags: String?,
+    val category: String?,
+    val iba: String?,
+    val alcoholic: String?,
+    val glass: String?,
+    val instructions: String?,
+    val thumb: String?,
+    val creativeCommonsConfirmed: String?,
+    val dateModified: String?,
+    val isFavorite: Boolean? = false
+)
+
+@Entity(
+    tableName = "ingredient",
+    foreignKeys = [ForeignKey(
+        entity = DrinkEntity::class,
+        parentColumns = arrayOf("id"),
+        childColumns = arrayOf("drinkId"),
+        onDelete = ForeignKey.CASCADE
+    )]
+)
+data class IngredientEntity(
+    @PrimaryKey val name: String,
+    val measure: String,
+    val drinkId: String
+)
