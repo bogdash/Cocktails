1e52c69f4d21d2b70067a6ee58793c8d2d9dfd10
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-06-07 22:05:41 +0300

    Update SaveCocktailByIdUseCase

--- domain/src/main/java/com/bogdash/domain/repository/CocktailRepository.kt
+++ domain/src/main/java/com/bogdash/domain/repository/CocktailRepository.kt
@@ -1,8 +1,10 @@
 package com.bogdash.domain.repository
 
 import com.bogdash.domain.models.Cocktails
+import com.bogdash.domain.models.Drink
 
 interface CocktailRepository {
     suspend fun getCocktailOfTheDay(): Cocktails
     suspend fun getCocktailDetailsById(id: String): Cocktails
+    suspend fun saveCocktail(drink: Drink)
 }
