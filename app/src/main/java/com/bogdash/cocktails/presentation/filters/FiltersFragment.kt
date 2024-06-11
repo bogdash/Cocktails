a9dd60eff0216c3007ba1ea0b45dec3fe0a65af6
Roman <tchernyshoff.roman2015@gmail.com>
2024-06-11 15:40:50 +0300

    - Create Fragment and ViewModel

--- app/src/main/java/com/bogdash/cocktails/presentation/filters/FiltersFragment.kt
+++ app/src/main/java/com/bogdash/cocktails/presentation/filters/FiltersFragment.kt
@@ -5,19 +5,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.bogdash.cocktails.Constants.HomeScreen.ALCOHOLIC
+import com.bogdash.cocktails.Constants.HomeScreen.NON_ALCOHOLIC
 import com.bogdash.cocktails.R
+import com.bogdash.cocktails.databinding.FragmentFiltersBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FiltersFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding: FragmentFiltersBinding
+    private val filtersViewModel: FiltersViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_filters, container, false)
+        binding = FragmentFiltersBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnClose.setOnClickListener{
+            parentFragmentManager.popBackStack()
+        }
+
+        binding.btnReset.setOnClickListener  {
+            with(binding) {
+                chipAlcoholic.isChecked
+                chipGroupIngredients.clearCheck()
+            }
+        }
+
+        binding.btnApply.setOnClickListener {
+            val typeAlcoholFilter: String
+            when(binding.chipGroupAlcohol.checkedChipId) {
+                0 -> typeAlcoholFilter = ALCOHOLIC
+                1 -> typeAlcoholFilter = NON_ALCOHOLIC
+                else -> typeAlcoholFilter = ALCOHOLIC
+            }
+            filtersViewModel.setFilterType(typeAlcoholFilter)
+            filtersViewModel.applyFilters()
+        }
     }
 
     companion object {
--- app/src/main/java/com/bogdash/cocktails/presentation/filters/FiltersViewModel.kt
+++ app/src/main/java/com/bogdash/cocktails/presentation/filters/FiltersViewModel.kt
@@ -1,3 +1,46 @@
 package com.bogdash.cocktails.presentation.filters
 
-class FiltersViewModel {}
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bogdash.cocktails.R
+import com.bogdash.domain.models.Cocktails
+import com.bogdash.domain.usecases.GetFilteredCocktailsByAlcoholTypeUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FiltersViewModel @Inject constructor(
+    private val getFilteredCocktailsByAlcoholTypeUseCase: GetFilteredCocktailsByAlcoholTypeUseCase
+) : ViewModel() {
+
+    private val _filteredCocktails = MutableLiveData<Cocktails>()
+    val filteredCocktails: LiveData<Cocktails> = _filteredCocktails
+
+    private val _filterType = MutableLiveData<String>()
+    val filterType: LiveData<String> = _filterType
+
+    private val _uiMessageChannel: MutableSharedFlow<Int> = MutableSharedFlow()
+    val uiMessageChannel = _uiMessageChannel.asSharedFlow()
+
+    fun setFilterType(type: String) {
+        _filterType.value = type
+    }
+
+    fun applyFilters() {
+        _filterType.value?.let { type ->
+            viewModelScope.launch {
+                try {
+                    val cocktails = getFilteredCocktailsByAlcoholTypeUseCase.execute(type)
+                    _filteredCocktails.value = cocktails
+                } catch (e: Exception) {
+                    _uiMessageChannel.emit(R.string.error_loading_cocktails)
+                }
+            }
+        }
+    }
+}
