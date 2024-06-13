469b7dfcb7dfe2c8fd1003f9321143b554a9b660
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-06-13 16:01:54 +0300

    Update status var color

--- app/src/main/java/com/bogdash/cocktails/presentation/main/MainActivity.kt
+++ app/src/main/java/com/bogdash/cocktails/presentation/main/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
@@ -33,6 +34,8 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
