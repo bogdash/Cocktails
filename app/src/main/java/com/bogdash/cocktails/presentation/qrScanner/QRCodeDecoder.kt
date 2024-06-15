568c84abbe3e9678c51660075d3f8dfba8617260
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-06-15 17:12:41 +0300

    Add documentation

--- app/src/main/java/com/bogdash/cocktails/presentation/qrScanner/QRCodeDecoder.kt
+++ app/src/main/java/com/bogdash/cocktails/presentation/qrScanner/QRCodeDecoder.kt
@@ -1,4 +1,4 @@
-package com.bogdash.cocktails.presentation.detail.qrCode
+package com.bogdash.cocktails.presentation.qrScanner
 
 import android.content.Context
 import android.graphics.Bitmap
@@ -9,38 +9,47 @@ import com.google.zxing.MultiFormatWriter
 import com.google.zxing.WriterException
 import com.google.zxing.common.BitMatrix
 
+/**
+ * QRCodeEncoder is responsible for encoding a given string into a QR code bitmap.
+ *
+ * @param context the context which the encoder is running
+ */
 class QRCodeEncoder(context: Context) {
     private val qrColor = ContextCompat.getColor(context, R.color.black)
     private val transparent = ContextCompat.getColor(context, R.color.transparent)
 
+    /**
+     * Encodes the specified content into a QR code bitmap.
+     *
+     * @param contentsToEncode the content to encode into the QR code
+     * @param size the width and height of the resulting QR code bitmap
+     * @return the generated QR code bitmap, or null if encoding fails
+     * @throws WriterException if an error occurs during encoding
+     */
     @Throws(WriterException::class)
     fun encodeAsBitmap(contentsToEncode: String?, size: Int): Bitmap? {
-        if (contentsToEncode == null) {
-            return null
-        }
-        val result: BitMatrix
-        try {
-            result = MultiFormatWriter().encode(
+        contentsToEncode ?: return null
+
+        val result: BitMatrix = try {
+            MultiFormatWriter().encode(
                 contentsToEncode,
                 BarcodeFormat.QR_CODE,
                 size,
                 size,
                 null
             )
-        } catch (iae: IllegalArgumentException) {
+        } catch (e: IllegalArgumentException) {
             return null
         }
 
-        val pixels = IntArray(size * size)
-        for (y in 0 until size) {
-            val offset = y * size
-            for (x in 0 until size) {
-                pixels[offset + x] = if (result[x, y]) qrColor else transparent
-            }
+        val pixels = IntArray(size * size) { index ->
+            val x = index % size
+            val y = index / size
+            if (result[x, y]) qrColor else transparent
         }
 
-        val bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888)
-        bitmap.setPixels(pixels, 0, size, 0, 0, size, size)
-        return bitmap
+        return Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888).apply {
+            setPixels(pixels, 0, size, 0, 0, size, size)
+        }
     }
 }
