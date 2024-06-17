4f04ca2dffe97ce38fe40c307402a8cabe2667e8
Roman <tchernyshoff.roman2015@gmail.com>
2024-06-17 13:39:59 +0300

    - add transaction lib

--- app/src/main/java/com/bogdash/cocktails/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/bogdash/cocktails/presentation/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
