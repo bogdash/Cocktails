d87c847055311cc504a76def31bf8446bceaf1d5
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-06-19 19:39:53 +0300

    Add documentation for ActivityCapture

--- app/src/main/java/com/bogdash/cocktails/presentation/qrScanner/ActivityCapture.kt
+++ app/src/main/java/com/bogdash/cocktails/presentation/qrScanner/ActivityCapture.kt
@@ -2,4 +2,7 @@ package com.bogdash.cocktails.presentation.qrScanner
 
 import com.journeyapps.barcodescanner.CaptureActivity
 
+/**
+ * ActivityCapture is a wrapper for QR code scanning activity.
+ */
 class ActivityCapture : CaptureActivity()
